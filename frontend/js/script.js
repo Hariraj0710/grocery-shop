f37affd90ce27dc3732c847bd37b58a0ff96d574
Hariraj0710 <hari07spider@gmail.com>
2026-03-29 07:47:12 +0530

    Update API URLs

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api/products';
+const API_URL = 'https://grocery-shop-cs8f.onrender.com/api/products';
 
 // On products page load
 document.addEventListener('DOMContentLoaded', () => {
